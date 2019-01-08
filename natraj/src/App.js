06c58b5db3d31df8f5c23cd2b31ff0bf31e217a4
Venkatnatraj <t.venkatanatraj95@gmail.com>
2019-01-08 18:25:54 +0530

    fetched the data from api

--- natraj/src/App.js
+++ natraj/src/App.js
@@ -19,15 +19,15 @@ class App extends React.Component {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    // const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
     if (city && country) {
       this.setState({
-        temperature: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
+        // temperature: data.main.temp,
+        // city: data.name,
+        // country: data.sys.country,
+        // humidity: data.main.humidity,
+        // description: data.weather[0].description,
         error: ""
       });
     } else {
